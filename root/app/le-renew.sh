bf3b06721246c48843c9611b7f14aaa9177f41fe
Ruben Trancoso <rubentrancoso@gmail.com>
2017-10-07 02:39:58 -0300

    preparing .crt certificates for convenience (eg.: docker registry)

--- root/app/le-renew.sh
+++ root/app/le-renew.sh
@@ -5,4 +5,18 @@ echo
 echo "<------------------------------------------------->"
 echo "cronjob running on "$(date)
 echo "Running certbot renew"
-certbot -n renew --renew-with-new-domains --allow-subset-of-names --standalone --pre-hook "s6-svc -d /var/run/s6/services/nginx" --post-hook "s6-svc -u /var/run/s6/services/nginx ; cd /config/keys/letsencrypt && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:"
+
+# setting certbot as staging
+STAGINGPARAM=""
+if [ "$STAGING" = "true" ]; then
+  STAGINGPARAM="--staging"
+fi
+
+certbot $STAGINGPARAM -n renew --renew-with-new-domains --allow-subset-of-names --standalone --pre-hook "s6-svc -d /var/run/s6/services/nginx" --post-hook "s6-svc -u /var/run/s6/services/nginx ; cd /config/keys/letsencrypt && openssl pkcs12 -export -out privkey.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:"
+
+echo "preparing .crt certificates for convenience (perhaps for a docker registry)"
+cd /config/keys/letsencrypt || exit
+cp privkey.pem domain.key
+cat cert.pem chain.pem > domain.crt
+chmod 777 domain.crt
+chmod 777 domain.key
